e22e1125290e0ed57eccc102190ef97280655ce8
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-03-24 02:28:23 +0700

    WIP Homepage

--- src/@type/common.ts
+++ src/@type/common.ts
@@ -1,2 +1,3 @@
 export type MainVariantProps = 'block' | 'outline' | 'unstyled';
 export type MainColorProps = 'primary' | 'secondary' | 'tertiary' | 'light';
+export type HeadingLevelProps = 'h1' | 'h2' | 'h3' | 'h4'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Container} from "react-bootstrap";
+import HeadingOffset from "@/components/common/heading/headingOffset/HeadingOffset";
 
 export type IndexProps = {};
 
 const Index = ({}: IndexProps): React.ReactElement => {
-    return (
-        <>INDEX</>
-    );
+    return <>
+        <Container as='section'>
+            <HeadingOffset className='text-center'>Meeting <span className="fc--tertiary">new people</span>,
+                having <span className='fc--secondary'>new experiences</span>, connecting
+                with each other — that’s what <span className="fc--primary">dating</span> should be
+                about!</HeadingOffset>
+        </Container>
+    </>
 }
 
 export default Index;
