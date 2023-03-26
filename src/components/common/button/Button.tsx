7bdc173b0a0b736a1189845756812aa2c38ada1b
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-03-26 22:12:48 +0700

    Adjust button and input

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import Link from "next/link";
 
-import type {MainColorProps, MainVariantProps} from "@/@type/common";
+import type {MainColorProps, MainSizeProps, MainVariantProps} from "@/@type/common";
+
+type ButtonContainerProps = {
+    className?: string;
+    children: React.ReactNode;
+}
+
+export const ButtonContainer = ({className, children}: ButtonContainerProps): React.ReactElement => (
+    <div className={className}>
+        {children}
+    </div>
+)
 
 type AnchorProps = {
     link: {
@@ -23,8 +34,9 @@ type ButtonActionProps = {
 export type ButtonProps = {
     className?: string;
     option?: {
-        variant: MainVariantProps;
+        variant: Pick<MainVariantProps, 'block' | 'outline' | 'unstyled'>;
         color: MainColorProps;
+        size?: Pick<MainSizeProps, 'md' | 'lg'>;
         fullWidth?: boolean;
     };
     event?: {
@@ -37,7 +49,8 @@ const Button = ({button, className, event, link, option}: ButtonProps): React.Re
 
     const btnStyle = option?.variant ? ` btn--${option.variant}` : ''
     const btnColor = option?.color ? ` btn--${option.color}` : '';
-    const btnClass = `${option?.variant !== 'unstyled' ? 'btn' : ''}${btnStyle}${btnColor}${option?.fullWidth ? ' w-100' : ''}${className ? ` ${className}` : ''}`;
+    const btnSize = (option?.size && option?.size !== 'md') ? ` btn--${option.size}` : '';
+    const btnClass = `${option?.variant !== 'unstyled' ? 'btn' : ''}${btnStyle}${btnColor}${btnSize}${option?.fullWidth ? ' w-100' : ''}${className ? ` ${className}` : ''}`;
 
     const buttonWrapperProps = link ? {
         ...btnClass ? {className: btnClass} : {},
--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import type {MainColorProps, MainVariantProps} from "@/@type/common";
+import {MainSizeProps} from "@/@type/common";
 
 export type InputProps = {
     className?: string;
     option?: {
-        variant?: MainVariantProps;
+        variant?: Pick<MainVariantProps, 'outline' | 'regular'>;
         color?: MainColorProps;
+        size?: Pick<MainSizeProps, 'md' | 'lg'>;
         align?: 'start' | 'center' | 'end';
+        isMultiline?: boolean | number;
         // fullWidth?: boolean;
     };
     input: {
@@ -19,17 +22,27 @@ export type InputProps = {
 };
 
 const Input = ({className, option, input}: InputProps): React.ReactElement => {
-    const formStyle = option?.variant ? ` form--${option.variant}` : '';
+    const formStyle = option?.variant ? `form--${option.variant}` : '';
     const formColor = option?.color ? ` form--${option.color}` : '';
-    const formClass = `${formStyle}${formColor}${className ? ` ${className}` : ''}`
+    const formSize = (option?.size && option?.size !== 'md') ? ` form--${option.size}` : '';
+    const formClass = `${formStyle}${formColor}${formSize}${className ? ` ${className}` : ''}`
+
+    const InputBlock = option?.isMultiline ? 'textarea' : 'input'
 
     return (
         <div className={formClass}>
             {input.label && <label
                 htmlFor={input.id}
                 className="form-label">{input.label}</label>}
-            <input
-                type={input?.type ?? 'text'}
+            {/*<input*/}
+            {/*    type={input?.type ?? 'text'}*/}
+            {/*    className={`form-control${option?.align ? ` text-${option.align}` : ''}`}*/}
+            {/*    id={input.id}*/}
+            {/*    placeholder={input.placeholder} />*/}
+            <InputBlock
+                // type={input?.type ?? 'text'}
+                {...!option?.isMultiline && {type: input?.type ?? 'text'}}
+                {...option?.isMultiline && {rows: typeof option.isMultiline === 'number' ? option.isMultiline : 5}}
                 className={`form-control${option?.align ? ` text-${option.align}` : ''}`}
                 id={input.id}
                 placeholder={input.placeholder} />
