22a87d41a3dc3d5cdecc4fea83898dc2416900b7
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-04-05 01:57:34 +0700

    Adjust faqs page

--- src/pages/faqs.tsx
+++ src/pages/faqs.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
-import {FAQS} from "@/data/mock/faqs";
+import {FAQS, HEADER} from "@/data/mock/faqs";
 
 import {Container} from "react-bootstrap";
 import Accordion from "@/components/common/accordion/Accordion";
 import HeadingOffset from "@/components/common/heading/headingOffset/HeadingOffset";
+import HeaderBanner from "@/components/common/banner/headerBanner/HeaderBanner";
+import ListBullet from "@/components/common/list/listBullet/ListBullet";
 
 export type FaqsProps = {};
 
 const Faqs = ({}: FaqsProps): React.ReactElement => (
     <>
+        <HeaderBanner
+            className='header-banner--faqs'
+            color='tertiary'
+            breadcrumb={HEADER.breadcrumb}>
+            <HeadingOffset
+                className='mb-4 text-center text-md-start'
+                option={{level: "h1", variant: "regular"}}>Frequently Asked Questions</HeadingOffset>
+            <ListBullet
+                className='mt-5 text-center text-md-start'
+                items={HEADER.list} />
+        </HeaderBanner>
+
         {Object.keys(FAQS).map((key: string) => (
             <Container
                 key={key}
