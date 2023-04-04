5baf32c7cc0c643b76803a5ea7d0bbf9d5481bf3
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-04-04 23:31:50 +0700

    adjust mock data faq

--- src/data/mock/faqs.ts
+++ src/data/mock/faqs.ts
@@ -1,8 +1,37 @@
 import {AccordionProps} from "@/components/common/accordion/Accordion";
 
-export const FAQS: Record<string, AccordionProps> = {
+export const FAQS: Record<'GENERAL' | 'LURKERS' | 'INFLUENCER' | string, ({ title: string } & AccordionProps)> = {
     GENERAL: {
+        title: 'General',
         color: 'tertiary',
+        items: [
+            {
+                title: 'How is my privacy protected?',
+                description: `<p>Elitely takes privacy protection very seriously. We will not disclose your personal information to any third party, not even to your date -- without your consent. Likewise, your date is not obligated to share his/her contact details with you.</p>`
+            },
+            {
+                title: 'How is my privacy protected?',
+                description: `<p>Elitely takes privacy protection very seriously. We will not disclose your personal information to any third party, not even to your date -- without your consent. Likewise, your date is not obligated to share his/her contact details with you.</p>`
+            },
+        ]
+    },
+    LURKERS: {
+        title: 'For Lurkers',
+        color: 'primary',
+        items: [
+            {
+                title: 'How is my privacy protected?',
+                description: `<p>Elitely takes privacy protection very seriously. We will not disclose your personal information to any third party, not even to your date -- without your consent. Likewise, your date is not obligated to share his/her contact details with you.</p>`
+            },
+            {
+                title: 'How is my privacy protected?',
+                description: `<p>Elitely takes privacy protection very seriously. We will not disclose your personal information to any third party, not even to your date -- without your consent. Likewise, your date is not obligated to share his/her contact details with you.</p>`
+            },
+        ]
+    },
+    INFLUENCER: {
+        title: 'For Personalities/Influencers',
+        color: 'secondary',
         items: [
             {
                 title: 'How is my privacy protected?',
--- src/pages/faqs.tsx
+++ src/pages/faqs.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import Accordion from "@/components/common/accordion/Accordion";
 import {FAQS} from "@/data/mock/faqs";
 import {Container} from "react-bootstrap";
+import HeadingOffset from "@/components/common/heading/headingOffset/HeadingOffset";
 
 export type FaqsProps = {};
 
 const Faqs = ({}: FaqsProps): React.ReactElement => (
     <>
-        <Container className='my-5'>
-            <Accordion
-                color={FAQS.GENERAL.color}
-                items={FAQS.GENERAL.items} />
-        </Container>
+        {Object.keys(FAQS).map((key: string) => (
+            <Container
+                key={key}
+                className='section-faqs'>
+                <HeadingOffset
+                    option={{variant: "regular"}}
+                    className='mb-4 text-center text-lg-start'>{FAQS[key].title}</HeadingOffset>
+                <Accordion
+                    color={FAQS[key].color}
+                    items={FAQS[key].items} />
+            </Container>
+        ))}
     </>
 );
 
