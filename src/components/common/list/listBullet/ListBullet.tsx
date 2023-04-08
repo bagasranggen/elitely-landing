0eee4bf1b841025f5dff7ab73acede834c0e1aba
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-04-08 14:46:16 +0700

    Update list bullet component

--- src/components/common/list/listBullet/ListBullet.tsx
+++ src/components/common/list/listBullet/ListBullet.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
+import type {MainColorProps} from "@/@type/common";
+
 export type ListBulletItemProps = string;
 
 export type ListBulletProps = {
     className?: string;
+    options?: {
+        color?: MainColorProps;
+        variant?: 'inline' | 'regular';
+    };
     items: ListBulletItemProps[]
 };
 
-const ListBullet = ({className, items}: ListBulletProps): React.ReactElement => (
-    <ul className={`list-unstyled list-bullet${className ? ` ${className}` : ''}`}>
-        {items.map((item: ListBulletItemProps) => (
-            <li key={item}>{item}</li>
-        ))}
-    </ul>
-);
+const ListBullet = ({className, options, items}: ListBulletProps): React.ReactElement => {
+    const listColor = options?.color ? ` list-bullet--${options.color}` : '';
+    const listVariant = options?.variant ? ` list-bullet--${options.variant}` : ''
+    const listClass = `list-unstyled list-inline list-bullet${listVariant}${listColor}${className ? ` ${className}` : ''}`
+
+    return (
+        <ul className={listClass}>
+            {items.map((item: ListBulletItemProps) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    )
+};
 
 export default ListBullet;
