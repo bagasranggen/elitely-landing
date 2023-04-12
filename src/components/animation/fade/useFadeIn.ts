d406a5219b49c1bc49d296ed4099beb0aab62c13
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-04-12 22:23:55 +0700

    Adjust animation

--- src/components/animation/fade/useFadeIn.ts
+++ src/components/animation/fade/useFadeIn.ts
@@ -1,4 +1,5 @@
-import {useEffect} from "react";
+import {useEffect, useLayoutEffect} from "react";
+import {useRouter} from "next/router";
 
 import {FADE_IN_DIRECTION} from "@/components/animation/fade/direction";
 
@@ -16,38 +17,55 @@ export type FadeInDirectionItemProps = {
 export type FadeInPositionProps = 'top' | 'center';
 
 const useFadeIn = () => {
-    useEffect(() => {
-        const context = gsap.context(() => {
-            // setTimeout(() => {
-            gsap.utils.toArray('[data-animation="fade-in"]').forEach((fade: any) => {
-                const dir: FadeInDirectionProps = fade?.getAttribute('data-animation-direction');
-                const delay: number | undefined = fade?.getAttribute('data-animation-delay') ? parseFloat(fade?.getAttribute('data-animation-delay')) : undefined;
-                const isOnTop = fade?.getAttribute('data-animation-position') === 'top';
+    const router = useRouter();
 
-                gsap.timeline({
-                    scrollTrigger: {
-                        trigger: fade,
-                        start: `top ${isOnTop ? 'bottom' : '80%'}`,
-                        // markers: true
-                        markers: process.env.NEXT_PUBLIC_GSAP_MARKER === '1'
-                    },
-                    // duration: .5,
-                    // ease: 'power2'
-                    ease: 'Power1.easeIn',
-                    ...delay ? {duration: delay} : {}
-                }).fromTo(fade, {
-                    opacity: 0,
-                    ...dir ? {[FADE_IN_DIRECTION[dir].direction]: FADE_IN_DIRECTION[dir][FADE_IN_DIRECTION[dir].direction]} : {}
-                }, {
-                    opacity: 1,
-                    ...dir ? {[FADE_IN_DIRECTION[dir].direction]: 0} : {}
-                })
+    const createFadeIn: any = () => {
+        return gsap.utils.toArray('[data-animation="fade-in"]').forEach((fade: any) => {
+            const dir: FadeInDirectionProps = fade?.getAttribute('data-animation-direction');
+            const delay: number | undefined = fade?.getAttribute('data-animation-delay') ? parseFloat(fade?.getAttribute('data-animation-delay')) : undefined;
+            const isOnTop = fade?.getAttribute('data-animation-position') === 'top';
+
+            gsap.timeline({
+                scrollTrigger: {
+                    trigger: fade,
+                    start: `top ${isOnTop ? 'bottom' : '80%'}`,
+                    // markers: true
+                    markers: process.env.NEXT_PUBLIC_GSAP_MARKER === '1'
+                },
+                // duration: .5,
+                // ease: 'power2'
+                ease: 'Power1.easeIn',
+                ...delay ? {duration: delay} : {}
+            }).fromTo(fade, {
+                opacity: 0,
+                ...dir ? {[FADE_IN_DIRECTION[dir].direction]: FADE_IN_DIRECTION[dir][FADE_IN_DIRECTION[dir].direction]} : {}
+            }, {
+                opacity: 1,
+                ...dir ? {[FADE_IN_DIRECTION[dir].direction]: 0} : {}
             })
-        });
-        // }, 5)
+        })
+    }
+
+    useLayoutEffect(() => {
+        const context = gsap.context(() => createFadeIn);
 
         return () => context.revert();
     }, [])
+
+    useLayoutEffect(() => {
+        const handleStart = () => ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+        const handleEnd = () => createFadeIn()
+
+        router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleEnd);
+        router.events.on("routeChangeError", handleEnd);
+
+        return () => {
+            router.events.off("routeChangeStart", handleStart);
+            router.events.off("routeChangeComplete", handleEnd);
+            router.events.off("routeChangeError", handleEnd);
+        };
+    }, [router.events]);
 }
 
 export default useFadeIn;
