f8bdf134873ce7cb483a7ce25d80dadc5f221d2e
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-04-17 21:06:43 +0700

    Adjust navigation

--- src/components/layout/navigation/Navigation.tsx
+++ src/components/layout/navigation/Navigation.tsx
@@ -22,13 +22,25 @@ const Navigation = forwardRef<HTMLElement, NavigationProps>((props, ref) => {
     const [expanded, setExpanded] = useState<boolean>(false);
 
     const navbarToggleHandler = (open: boolean) => {
-        setExpanded(open);
-        if (!open) {
-            const body = document.querySelector('body');
+        const body = document.querySelector('body');
 
-            if (body) body.style.overflow = '';
-        }
+        if (body) body.style.overflow = open ? 'hidden' : '';
+        setExpanded(open);
     }
+
+    useEffect(() => {
+        const handleEnd = () => navbarToggleHandler(false);
+
+        // router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleEnd);
+        router.events.on("routeChangeError", handleEnd);
+
+        return () => {
+            // router.events.off("routeChangeStart", handleStart);
+            router.events.off("routeChangeComplete", handleEnd);
+            router.events.off("routeChangeError", handleEnd);
+        };
+    }, [router.events])
 
     return (
         <Navbar
@@ -38,12 +50,7 @@ const Navigation = forwardRef<HTMLElement, NavigationProps>((props, ref) => {
             expand="lg"
             sticky='top'
             expanded={expanded}
-            // collapseOnSelect={true}
-            onToggle={(expanded: boolean) => {
-                const body = document.querySelector('body');
-
-                if (body) body.style.overflow = expanded ? 'hidden' : '';
-            }}>
+            onToggle={(expanded: boolean) => navbarToggleHandler(expanded)}>
             <Container>
                 <Col xs='auto'>
                     <Navbar.Brand
@@ -64,62 +71,22 @@ const Navigation = forwardRef<HTMLElement, NavigationProps>((props, ref) => {
                                 key={nav.uri}
                                 as={Link}
                                 className={`${nav?.options?.isBold ? 'fw-bold' : ''}${nav.uri === asPath ? ' active' : ''}`}
-                                href={nav.uri}
-                                onClick={() => isMobile && navbarToggleHandler(false)}>{nav.label}</Nav.Link>
+                                href={nav.uri}>{nav.label}</Nav.Link>
                         ))}
                     </Nav>
                     <Nav className='align-items-center'>
                         <Button
                             option={{variant: 'outline', color: 'primary'}}
-                            link={{href: '#', label: 'Login'}}
-                            event={{onClick: () => isMobile && navbarToggleHandler(false)}} />
+                            link={{href: '#', label: 'Login'}} />
                         <Button
                             option={{variant: 'block', color: 'light'}}
-                            link={{href: '/contact-us', label: 'Contact Us'}}
-                            event={{onClick: () => isMobile && navbarToggleHandler(false)}} />
+                            link={{href: '/contact-us', label: 'Contact Us'}} />
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     )
 })
-
-// const Navigation = ({}: NavigationProps): React.ReactElement => (
-//     <Navbar
-//         className='navbar--primary'
-//         variant='dark'
-//         expand="lg">
-//         <Container>
-//             <Col xs='auto'>
-//                 <Navbar.Brand href="#home">
-//                     <Image
-//                         src={logo}
-//                         alt='elitely' />
-//                 </Navbar.Brand>
-//             </Col>
-//             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//             <Navbar.Collapse id="basic-navbar-nav">
-//                 <Nav className="mx-auto">
-//                     {MAIN_NAVIGATION.map((nav: any) => (
-//                         <Nav.Link
-//                             key={nav.uri}
-//                             className={nav.uri === '/' ? 'active' : ''}
-//                             href={nav.uri}>{nav.label}</Nav.Link>
-//                     ))}
-//                 </Nav>
-//                 <Nav className='align-items-center'>
-//                     <Button
-//                         option={{variant: 'outline', color: 'primary'}}
-//                         link={{href: '#', label: 'Login'}} />
-//                     <Button
-//                         option={{variant: 'block', color: 'light'}}
-//                         button={{type: 'button', label: 'Contact Us'}} />
-//                 </Nav>
-//             </Navbar.Collapse>
-//         </Container>
-//     </Navbar>
-//
-// );
 
 Navigation.displayName = 'Navigation'
 export default Navigation;
