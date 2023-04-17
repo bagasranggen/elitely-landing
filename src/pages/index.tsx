a06ef6aab6b2ab4930b02c1abb865399b43ef87a
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-04-17 22:30:51 +0700

    Add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 
 import {Container} from "react-bootstrap";
 import HeadingOffset from "@/components/common/heading/headingOffset/HeadingOffset";
+import HeaderHead from "@/components/layout/header/headerHead/HeaderHead";
 
 export type IndexProps = {};
 
-const Index = ({}: IndexProps): React.ReactElement => {
-    return <>
+const Index = ({}: IndexProps): React.ReactElement => (
+    <>
+        <HeaderHead />
+
         <Container className='my-5'>
             <HeadingOffset
                 className='text-center'
                 option={{level: "h1", animation: {position: 'top'}}}>This is sign up page</HeadingOffset>
         </Container>
     </>
-}
+)
 
 export default Index;
