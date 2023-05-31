494dbc5e82edd5bb9515c246619bc1b84f34892b
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-06-01 00:59:28 +0700

    Adjust footer show animation trigger

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import Image from "next/image";
 
-import {createAnimation} from "@/components/animation/helper";
+import { createAnimation } from "@/components/animation/helper";
 
 import logo from "@/assets/images/logo.svg";
 
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Button from "@/components/common/button/Button";
 import Input from "@/components/common/input/Input";
 
 export type FooterProps = {};
 
 const Footer = ({}: FooterProps): React.ReactElement => (
-    <footer {...createAnimation({type: 'fade-in', direction: "up"})}>
+    <footer {...createAnimation({ type: 'fade-in', direction: "up", position: "top" })}>
         <Container>
-            <Row className='gy-4 justify-content-between'>
+            <Row className="gy-4 justify-content-between">
                 <Col md={8}>
                     <Image
                         src={logo}
-                        alt='elitely' />
+                        alt="elitely" />
                     <p>The world’s first social media platform where users can pay a small fee to
                         unlock chats and meets
                         ups with other users on our platform, who have been qualified as interesting Personalities or
@@ -28,12 +28,12 @@ const Footer = ({}: FooterProps): React.ReactElement => (
                     <h2>Newsletter</h2>
                     <form action="">
                         <Input
-                            className='mt-3 mb-3'
-                            option={{variant: 'outline', color: 'light', size: 'lg', align: 'center'}}
-                            input={{id: 'subscribe', placeholder: 'Your email'}} />
+                            className="mt-3 mb-3"
+                            option={{ variant: 'outline', color: 'light', size: 'lg', align: 'center' }}
+                            input={{ id: 'subscribe', placeholder: 'Your email' }} />
                         <Button
-                            option={{variant: 'block', color: 'light', size: 'lg', fullWidth: true}}
-                            button={{type: 'submit', label: 'Subscribe'}} />
+                            option={{ variant: 'block', color: 'light', size: 'lg', fullWidth: true }}
+                            button={{ type: 'submit', label: 'Subscribe' }} />
                     </form>
                 </Col>
             </Row>
